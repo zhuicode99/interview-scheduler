c660d2eaf3f03568926fd06f7d77096226f8b07b
zhuicode99 <enlin291814833@hotmail.com>
2023-02-27 16:25:34 -0500

    add daylist component and stories

---
+++ src/components/DayList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+
+export default function DayList(props){
+
+  return(//每个stories里面的add都会展示一遍以下所有html
+    <ul>
+      <DayListItem 
+        key={props.days[0].id}
+        name={props.days[0].name} 
+        spots={props.days[0].spots} 
+        selected={props.days[0].name === props.day}
+        setDay={props.setDay}  
+      />
+      <DayListItem
+        key={props.days[1].id} 
+        name={props.days[1].name} 
+        spots={props.days[1].spots} 
+        selected={props.days[1].name === props.day}
+        setDay={props.setDay}  
+      />
+      <DayListItem 
+        key={props.days[2].id}
+        name={props.days[2].name}
+        spots={props.days[2].spots} 
+        selected={props.days[2].name === props.day}
+        setDay={props.setDay}  
+      />      
+    </ul>
+  )
+
+}
--- stories/index.js
+++ stories/index.js
@@ -7,6 +7,8 @@ import "index.scss";
 
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
+import DayList from "components/DayList"
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -35,3 +37,36 @@ storiesOf("Button", module)
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   )); // 测试不同状态下的daylistitem和button（上面）
   //component里的code是单独分别针对每一条add的。
+
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+  
+  storiesOf("DayList", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+    })
+    .add("Monday", () => (
+      <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+    ))
+    .add("Tuesday", () => (
+      <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+    ))
+    .add("Wednesday", () => (
+        <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+    ));
+  
