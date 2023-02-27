509b01cfb83f7d1f097ef7bd139cf1a724a64492
zhuicode99 <enlin291814833@hotmail.com>
2023-02-27 16:39:34 -0500

    refactor daylist component using map function

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,30 +4,21 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props){
 
-  return(//每个stories里面的add都会展示一遍以下所有html
-    <ul>
+  const dayListItems = props.days.map((day) => {
+    return(
+      <ul>
       <DayListItem 
-        key={props.days[0].id}
-        name={props.days[0].name} 
-        spots={props.days[0].spots} 
-        selected={props.days[0].name === props.day}
+        key={day.id}
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props.day}
         setDay={props.setDay}  
       />
-      <DayListItem
-        key={props.days[1].id} 
-        name={props.days[1].name} 
-        spots={props.days[1].spots} 
-        selected={props.days[1].name === props.day}
-        setDay={props.setDay}  
-      />
-      <DayListItem 
-        key={props.days[2].id}
-        name={props.days[2].name}
-        spots={props.days[2].spots} 
-        selected={props.days[2].name === props.day}
-        setDay={props.setDay}  
-      />      
     </ul>
+  )
+})
+  return(
+    dayListItems
   )
 
 }
