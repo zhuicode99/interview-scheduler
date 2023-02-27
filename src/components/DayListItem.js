11f58517feba3fa4a4f2a6ee3af781c2a17c896d
zhuicode99 <enlin291814833@hotmail.com>
2023-02-27 15:20:40 -0500

    add formatspots function for spots remaining msg

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,10 +6,22 @@ export default function DayListItem(props) {
 
 let dayListItemClass = classNames("day-list__item", {"day-list__item--selected":props.selected, "day-list__item--full":props.spots===0})
 
+const formatSpots = (spots)=> {
+
+  if (spots === 0) {
+   return "no spots remaining"
+  } else if (spots === 1) {
+   return "1 spot remaining"
+  } else {
+   return `${spots} spots remaining`
+  };
+}
+
+
   return (
     <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
